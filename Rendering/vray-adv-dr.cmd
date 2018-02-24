1c49810d9242c40451b5d70371cbfe8077e9efc0
Christian Smith <christian.smith@microsoft.com>
2018-02-25 10:26:36 +1100

    Install backburner

--- Rendering/vray-adv-dr.cmd
+++ Rendering/vray-adv-dr.cmd
@@ -4,5 +4,8 @@ rem Create a config file to tell vrayspawner about the new location of IO
 echo [Directories]> "C:\Autodesk\3ds Max 2018\vrayspawner.ini"
 echo AppName=C:\Autodesk\3ds Max 2018\3dsmaxio.exe>> "C:\Autodesk\3ds Max 2018\vrayspawner.ini"
 
+rem Install Backburner if available.
+msiexec /i Backburner.msi /qn
+
 rem start vray spawner
 "C:\Autodesk\3ds Max 2018\vrayspawner2018.exe" -port=%port%
