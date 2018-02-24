9d9baab94a1749f8265adeb3615ca922da534a13
Christian Smith <christian.smith@microsoft.com>
2018-02-25 10:30:29 +1100

    Wait for msiexec install.

--- Rendering/vray-adv-dr.cmd
+++ Rendering/vray-adv-dr.cmd
@@ -5,7 +5,7 @@ echo [Directories]> "C:\Autodesk\3ds Max 2018\vrayspawner.ini"
 echo AppName=C:\Autodesk\3ds Max 2018\3dsmaxio.exe>> "C:\Autodesk\3ds Max 2018\vrayspawner.ini"
 
 rem Install Backburner if available.
-msiexec /i Backburner.msi /qn
+start /wait msiexec /i Backburner.msi /qn
 
 rem start vray spawner
 "C:\Autodesk\3ds Max 2018\vrayspawner2018.exe" -port=%port%
